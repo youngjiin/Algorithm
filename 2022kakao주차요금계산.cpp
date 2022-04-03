e37de89899867e7804717bca454a3983797fe9ad
youngjiin <32324688+youngjiin@users.noreply.github.com>
2022-04-03 19:30:33 +0900

    Update 2022kakao주차요금계산.cpp

--- 2022kakao주차요금계산.cpp
+++ 2022kakao주차요금계산.cpp
@@ -12,9 +12,9 @@ int get_ftime(int In_htime, int In_mtime, int h_time, int m_time){
         m_time+=60;
     }
     int du_h = h_time - In_htime;
-	int du_m = m_time - In_mtime;
+    int du_m = m_time - In_mtime;
     int f_time = du_h*60 + du_m;
-	return f_time;
+    return f_time;
 }
 vector<int> solution(vector<int> fees, vector<string> records) {
     vector<int> answer;
@@ -27,7 +27,7 @@ vector<int> solution(vector<int> fees, vector<string> records) {
     int stand_fee = fees[3];
     
     unordered_map<int,pair<pair<int,int>,int>> num2index; // <차량번호, <<들어온시간,들어온분>,시간>>
-	  unordered_map<int, int> InorOut; // <차량번호, in or out>
+    unordered_map<int, int> InorOut; // <차량번호, in or out>
     // out 이면 -1 로 reset
     for(int i=0; i<records.size(); i++){
         int car_num = stoi(records[i].substr(6, 4));
@@ -48,31 +48,31 @@ vector<int> solution(vector<int> fees, vector<string> records) {
             int f_time = get_ftime(In_htime, In_mtime, h_time, m_time);
             
             num2index[car_num].second += f_time;
-			InorOut[car_num] = 1;
+	    InorOut[car_num] = 1;
         }
         
     }
     
-	  set<pair<int,int>> s;
+    set<pair<int,int>> s;
     for(auto it = InorOut.begin(); it!=InorOut.end(); it++){
-		int car_num = (*it).first;
+	int car_num = (*it).first;
         int f_time = num2index[car_num].second;
         if((*it).second == 0){
-			int h_time = 23;
-			int m_time = 59;
-			int In_htime = num2index[car_num].first.first;
+	    int h_time = 23;
+	    int m_time = 59;
+	    int In_htime = num2index[car_num].first.first;
             int In_mtime = num2index[car_num].first.second;
-			f_time += get_ftime(In_htime, In_mtime, h_time, m_time);
-		}
+	    f_time += get_ftime(In_htime, In_mtime, h_time, m_time);
+	}
         int fee = base_fee;
         if(f_time - base_time>0){
             fee += ceil(float(f_time - base_time) / float(stand_time))*stand_fee;
         }
         s.insert({car_num, fee});
-	}
+    }
     
-	for(auto it = s.begin(); it!=s.end(); it++){
-		answer.push_back((*it).second);
-	}
+    for(auto it = s.begin(); it!=s.end(); it++){
+	answer.push_back((*it).second);
+    }
     return answer;
 }
